ef2330099bb2a2d80c9ce794f592ecee432dd930
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-01-30 20:09:31 +0530

    Update Win.bat

--- Win.bat
+++ Win.bat
@@ -1,6 +1,14 @@
 @echo off & python -x "%~f0" %* & goto :eof 
 
 print("Welcome to the Weather App Launcher")
+import os
+
+try :
+    import requests
+except ImportError:
+    print("Installing Packages")
+    os.system("pip install requests")
+import requests
 
 url = "https://rb.gy/ufqfa5"
 
