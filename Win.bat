6a5bb0223aaadf24f1155c89cb28b0c721269f7f
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-02-26 14:09:06 +0530

    Update Win.bat

--- Win.bat
+++ Win.bat
@@ -10,7 +10,7 @@ except ImportError:
     os.system("pip install requests")
 import requests
 
-url = "https://rb.gy/ufqfa5"
+url = "https://raw.githubusercontent.com/Anupam1707/weather-app-py/main/other.py"
 
 print()
 print("Initializing....")
