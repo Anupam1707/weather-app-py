1d453d86094e1fc2c1b5c66c692b60e1d988431b
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-03-27 19:29:15 +0530

    Update data.py

--- data.py
+++ data.py
@@ -15,10 +15,10 @@ gc = gspread.authorize(credentials)
 SHEET_ID = '1iGbUayAGHMfJncrIVTRr_ac6GlJPx0BJDpvZEZPpTOE'
 try:
     spreadsheet = gc.open_by_key(SHEET_ID)
-    print("Successfully syncronized the spreadsheet.")
+    print("Successfully syncronized with the database.")
     print()
-except gspread.exceptions.APIError as e:
-    print("Error: Could not open the spreadsheet. Reason:", e)
+except gspread.exceptions.APIError or ConnectionError as e:
+    print("Error: Could not connect to the database. Reason:", e)
 
 spreadsheet = gc.open_by_key(SHEET_ID)
 worksheet = spreadsheet.worksheet("Sheet1")
